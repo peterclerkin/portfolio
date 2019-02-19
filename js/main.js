70eb37737552412f38619bb5d94c8a04df370745
Peter <clerkin07@gmail.com>
2019-02-19 17:17:57 +0000

    Menu Button Update

--- js/main.js
+++ js/main.js
@@ -13,8 +13,18 @@ mobileNav.addEventListener('click', toggleMenu);
 function toggleMenu() {
     if (!showMenu) {
         mobileNav.classList.add('close');
+        menuNav.classList.add('show');
+        menu.classList.add('show');
+        menuItems.forEach(item => item.classList.add('show'));
+        // Set Menu State
+        showMenu = true;
     } else {
-
+        mobileNav.classList.remove('close');
+        menuNav.classList.remove('show');
+        menu.classList.remove('show');
+        menuItems.forEach(item => item.classList.remove('show'));
+        // Set Menu State
+        showMenu = false;
     }
 
 }
